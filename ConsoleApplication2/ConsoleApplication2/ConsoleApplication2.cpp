1677d656a44edb0764a86d0ad3def3eb0467b5b2
Dodkoo23 <92848160+Dodkoo23@users.noreply.github.com>
2022-01-11 22:47:52 +0100

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Zadaj cislo od 1-3" << endl;
+    int cislo;
+    cin >> cislo;
+
+    switch (cislo) {
+     case 1:
+         cout << "Som hladny" << endl;
+         break;
+     case 2:
+         cout << "Do zbrane" << endl;
+         break;
+     case 3:
+         cout << "nananana" << endl;
+         break;
+     default:
+         cout << "Toto slovo nepoznám" << endl;
+         break;
+
+         return 0;
+
+    }
+}
+       
